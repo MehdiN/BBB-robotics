7a1dc8f2ef7ae9f3681aa349a6464211b329a660
MehdiN <nima.mehdi@ensta-bretagne.org>
2017-12-04 18:16:11 +0100

    Added mpu9250 IMU helper

---
+++ drivers/imu_mpu9250.h
@@ -0,0 +1,47 @@
+/* DRIVER FOR MPU-9250 */
+
+// MPU 9250 REGISTERIES
+
+
+// GYROSCOPE AND ACCELEROMETER REGISTER MAP 
+
+//Self tests and offsets REGISTERS
+
+#define SELF_TEST_X_GYRO 	0x00
+#define SELF_TEST_Y_GYRO 	0x01
+#define SELF_TEST_Z_GYRO 	0x02
+
+#define SELF_TEST_X_ACCEL 	0x0D
+#define SELF_TEST_Y_ACCEL 	0x0E
+#define SELF_TEST_Z_ACCEL 	0x0F
+
+#define XG_OFFSET_H			0x13		
+#define XG_OFFSET_L			0x14
+#define YG_OFFSET_H			0x15
+#define YG_OFFSET_L			0x16
+#define ZG_OFFSET_H			0x17
+#define ZG_OFFSET_L			0x18
+
+#define SMPLRT_DIV			0x19
+#define CONFIG 				0x1A
+#define GYRO_CONFIG			0x1B
+#define ACCEL_CONFIG 		0x1C
+#define ACCEL_CONFIG2		0x1D
+#define LP_ACCEL_ODR		0x1E
+#define WOM_THR				0x1F
+
+#define 
+#define
+#define
+#define
+
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+#define
